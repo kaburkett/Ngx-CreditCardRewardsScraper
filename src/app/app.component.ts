b0bd35bcc1bdaa765dc769d9f45318f037a7d83f
Kyle A. Burkett <kyle.burkett@wwinc.com>
2018-07-13 12:28:17 -0500

    add chase freedom card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,7 +15,16 @@ export class AppComponent implements OnInit {
   ngOnInit() {
     this.rewards.getRewards().subscribe(response => {
       this.bankInfo = response;
+      // Transform this to fit the rest... should move to the api side later
+      this.bankInfo[2][3] = this.toTitleCase(this.bankInfo[2][3]);
       console.log(this.bankInfo);
     });
   }
+
+  toTitleCase(str) {
+  str = str.substring(str.indexOf('.') + 1);
+  return str.replace(/\w\S*/g, function (txt) {
+    return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+  });
+}
 }
